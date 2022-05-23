290c6b41cb861e35d28251f3a572c2f60200045a
Divine Orji <dpkreativ@gmail.com>
2022-05-23 07:52:09 +0100

    configure gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,27 @@
+require('dotenv').config;
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    'gatsby-plugin-image',
+    {
+      resolve: `gatsby-plugin-remote-images`,
+      options: {
+        nodeType: 'cloudinaryMedia',
+        imagePath: 'url',
+      },
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
+      },
+    },
+  ],
+};
