440f16d6f308f6090817d070f7e70eebbed213eb
Divine Orji <dpkreativ@gmail.com>
2022-05-23 07:54:54 +0100

    add prefix to fetch cloudinary images from folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
         apiKey: process.env.CLOUDINARY_API_KEY,
         apiSecret: process.env.CLOUDINARY_API_SECRET,
         resourceType: `image`,
+        prefix: `gatsby-images/`,
       },
     },
   ],
